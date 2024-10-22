0bb867f2df23df811ba443ce99c96e737ce2d4e8
Zeeshan Amin <zeeshanamin013@gmail.com>
2024-10-22 14:17:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,64 @@ let resetButtoon=document.getElementById("reset-btn")
 function showTime(){
   seconds++
   if(seconds==60){
-    seconds=0
+    seconds=0let display = document.querySelector(".display")
+let buttons = document.querySelectorAll(".btn")
+let clearButton = document.querySelector(".clearbtn")
+
+function add(val) {
+    display.value += val
+}
+  
+function cleardisplay() {
+    display.value = ''
+    enableButtons();
+}
+
+function deletelast() {
+    display.value = display.value.slice(0, -1)
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+        if (display.value === Infinity || display.value === -Infinity) {
+            display.value = "Infinity"
+        }
+        if(display.value="Infinity"){
+disableButtons()
+        }else{
+            enableButtons()
+        }
+      
+    } catch (error) {
+        display.value = "Error";
+        disableButtons(); 
+    }
+}
+
+function clearError() {
+    if (display.value === "Error") {
+        display.value = "";
+        enableButtons();
+    }
+  
+}
+
+  
+
+
+function disableButtons() {
+    for (let button of buttons) {
+        button.disabled = true;
+    }
+}
+
+function enableButtons() {
+    for (let button of buttons) {
+        button.disabled = false;
+    }
+}
+
     minutes++
   }
   if(minutes==60){
